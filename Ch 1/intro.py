937e7625f5f3f7140b5b12ed4908df6ee721497b
Josephonate <131502920+Josephonate@users.noreply.github.com>
2024-04-07 00:10:57 -0400

    started python practice

---
+++ Ch 1/intro.py
@@ -0,0 +1,16 @@
+print('Hello World')
+
+
+message = "This is my message in python!!"
+
+print(message)
+
+def helloFunction():
+    print('this is my function')
+
+helloFunction()
+
+prompt = "What is your name? "
+name = input(prompt)
+
+print("hello " + name + " how are you?")
